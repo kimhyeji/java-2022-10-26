334b554997354c7a486c554fe5f62bc27b95b6f8
kimhyeji <gpwl5589@naver.com>
2022-10-31 20:45:59 +0900

    게시물 리스팅시 숫자 데이터의 출력 자리수 지정, 조회수 기능 추가

--- src/com/kor/java/proj/Main.java
+++ src/com/kor/java/proj/Main.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
+		
+//		makeTestArticles();
+		
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
@@ -44,11 +47,11 @@ public class Main {
 					continue;
 				}
 
-				System.out.println("번호  | 제목");
+				System.out.println("번호  | 조회  | 제목");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("%d    |  %s\n", article.id, article.title);
+					System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
 				}
 
 			} else if (command.startsWith("article detail ")) {
@@ -71,11 +74,14 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
+				
+				foundArticle.increaseHit();
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("조회 : %d\n", foundArticle.hit);
 
 			} else if (command.startsWith("article modify ")) {
 				String[] commandBits = command.split(" ");
@@ -142,6 +148,11 @@ public class Main {
 		sc.close();
 		System.out.println("== 프로그램 끝 ==");
 	}
+
+//	private static void makeTestArticles() {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
 
 class Article {
@@ -149,11 +160,17 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int hit;
 
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.hit = 0;
+	}
+	
+	public void increaseHit() {
+		hit++;
 	}
 }
