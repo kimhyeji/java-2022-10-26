8d4c5cbf39c66c68563d09c7412a4df272bba7a0
kimhyeji <gpwl5589@naver.com>
2022-11-09 20:25:14 +0900

    한번 사용된 게시물 번호가 재사용되지 않도록 lastId 도입

--- src/com/kor/java/proj/dao/MemberDao.java
+++ src/com/kor/java/proj/dao/MemberDao.java
@@ -3,12 +3,20 @@ package com.kor.java.proj.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.kor.java.proj.dto.Article;
 import com.kor.java.proj.dto.Member;
 
-public class MemberDao {
+public class MemberDao extends Dao {
 	public List<Member> members;
-	
+
 	public MemberDao() {
 		members = new ArrayList<>();
 	}
+	
+	public void add(Member member) {
+		members.add(member);
+		lastId = member.id;
+	}
+
+	
 }
